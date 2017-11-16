55cc014028f41e9876f70049f26114e827f1457a
Steve Bishop <xipooo@gmail.com>
2017-11-16 09:35:11 -0800

    Stage First Files

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter an number between 5 and 20.");
+            var myResponse = int.Parse(Console.ReadLine());
+            var i = 1;
+            while (i <= myResponse) {
+                Console.WriteLine(i + " is " + (i % 2 == 0 ? "even": (i % 3 == 0 ? "multiple of 3" : "Odd")));
+                i++;
+            }
+        }
+    }
+}
