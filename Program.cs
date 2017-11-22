52bfe0cc77e8859ee823cfeba43471615af9fdee
Steve Bishop <xipooo@gmail.com>
2017-11-22 13:01:43 -0800

    Generics Example in Main

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-using test.SingletonPattern;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace test
 {
@@ -7,7 +8,33 @@ namespace test
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(POTUS.Instance.FirstName);
+            /*ArrayList stringsArrayList = new ArrayList();
+            stringsArrayList.Add("Is");
+            stringsArrayList.Add("the");
+            stringsArrayList.Add("number");
+            stringsArrayList.Add(13); // Compiles but gives runtime error!
+            stringsArrayList.Add("unlucky");
+            stringsArrayList.Add("?");
+            Console.WriteLine("Using ArrayList");
+            foreach (string word in stringsArrayList)
+            {
+                Console.Write(word + " ");
+            }
+            Console.WriteLine();*/
+
+            List<string> stringsGenericList = new List<string>();
+            stringsGenericList.Add("Is");
+            stringsGenericList.Add("the");
+            stringsGenericList.Add("number");
+            //stringsGenericList.Add(13); // Gives compile error!
+            stringsGenericList.Add("unlucky");
+            stringsGenericList.Add("?");
+            Console.WriteLine("Using List<string>");
+            foreach (string word in stringsGenericList)
+            {
+                Console.Write(word + " ");
+            }
+            Console.WriteLine();
         }
     }
 }
